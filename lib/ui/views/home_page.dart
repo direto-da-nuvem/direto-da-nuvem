99f854160ae20923920beabff48d066d3f2b5492
MarcosFernando <marcosfernando@id.uff.br>
2023-08-31 19:31:22 -0300

    list tile de teste adicionado

--- lib/ui/views/home_page.dart
+++ lib/ui/views/home_page.dart
@@ -138,11 +138,252 @@ class _HomePageState extends State<HomePage> {
                 ),
               ],
             ),
-            body: Card(
-              child: Text("Sample Test Page"),
-              color: Colors.blue,
+            body: SingleChildScrollView(
+              child: ExpansionTile(
+                  title: Text('ExpansionTile 1'),
+                  subtitle: Text('Trailing expansion arrow icon'),
+                  children: <Widget>[
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.add),
+                          Text(" Provide sample"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Provide sample clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.search),
+                          Text(" Search"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Search clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.messenger_outline_sharp),
+                          Text(" Messages"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Messages clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    Container(
+                      color: Colors.white,
+                      child: const SizedBox(
+                        height: 5,
+                      ),
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.settings),
+                          Text(" Settings"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Settings clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.info),
+                          Text(" Smaple.io"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Sample.io clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          // TODO: evitar que quebre aundo o nome for muito grande
+                          Text("Marcos Fernando", softWrap: true),
+                          Icon(Icons.exit_to_app),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Exit clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.add),
+                          Text(" Provide sample"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Provide sample clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.search),
+                          Text(" Search"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Search clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.messenger_outline_sharp),
+                          Text(" Messages"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Messages clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    Container(
+                      color: Colors.white,
+                      child: const SizedBox(
+                        height: 5,
+                      ),
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.settings),
+                          Text(" Settings"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Settings clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.info),
+                          Text(" Smaple.io"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Sample.io clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          // TODO: evitar que quebre aundo o nome for muito grande
+                          Text("Marcos Fernando", softWrap: true),
+                          Icon(Icons.exit_to_app),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Exit clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.add),
+                          Text(" Provide sample"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Provide sample clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.search),
+                          Text(" Search"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Search clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.messenger_outline_sharp),
+                          Text(" Messages"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Messages clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    Container(
+                      color: Colors.white,
+                      child: const SizedBox(
+                        height: 5,
+                      ),
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.settings),
+                          Text(" Settings"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Settings clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          Icon(Icons.info),
+                          Text(" Smaple.io"),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Sample.io clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                    ListTile(
+                      title: const Row(
+                        children: [
+                          // TODO: evitar que quebre aundo o nome for muito grande
+                          Text("Marcos Fernando", softWrap: true),
+                          Icon(Icons.exit_to_app),
+                        ],
+                      ),
+                      onTap: () {
+                        debugPrint("Exit clicked");
+                        // Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+            ),
 
-            )
         );
       }
     );
