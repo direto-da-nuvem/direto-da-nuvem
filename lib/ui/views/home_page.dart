a1982152aecd5b2c3e836ef9d15949b8544347a4
MarcosFernando <marcosfernando@id.uff.br>
2023-08-31 15:55:59 -0300

    drawer funcinando

--- lib/ui/views/home_page.dart
+++ lib/ui/views/home_page.dart
@@ -5,8 +5,15 @@ import 'package:sample/controllers/home_controller.dart';
 // Itens do popMenuButton
 enum MenuItem { itemOne }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String selectedItem = "Dashboard";
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +22,126 @@ class HomePage extends StatelessWidget {
       builder: (homeController) {
         return Scaffold(
             appBar: AppBar(
-              title: const Text('Sample'),
-              // centerTitle: true,
-              actions: [],
+              // title: const Text('Sample For Science'),
+              centerTitle: true,
             ),
-            body: Text("Sample Test Page",
-              style: TextStyle(color: Colors.white)
+            drawer: Column(
+              children: [
+                Container(
+                  height: 600,
+                  child: Drawer(
+                    width: 170,
+                    backgroundColor: Color.fromARGB(201,209,242,255),
+                    child: ListView(
+                      padding: EdgeInsets.zero,
+                      children: [
+                        const DrawerHeader(
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(201,209,242,255),
+                          ),
+                          child: Text('Drawer Header'),
+                        ),
+                        ListTile(
+                          title: const Row(
+                            children: [
+                              Icon(Icons.apps),
+                              Text(" Dashboard"),
+                            ],
+                          ),
+                          onTap: () {
+                            debugPrint("Dashboard clicked");
+                            Navigator.pop(context);
+                          },
+                        ),
+                        ListTile(
+                          title: const Row(
+                            children: [
+                              Icon(Icons.add),
+                              Text(" Provide sample"),
+                            ],
+                          ),
+                          onTap: () {
+                            debugPrint("Provide sample clicked");
+                             Navigator.pop(context);
+                          },
+                        ),
+                        ListTile(
+                          title: const Row(
+                            children: [
+                              Icon(Icons.search),
+                              Text(" Search"),
+                            ],
+                          ),
+                          onTap: () {
+                            debugPrint("Search clicked");
+                             Navigator.pop(context);
+                          },
+                        ),
+                        ListTile(
+                          title: const Row(
+                            children: [
+                              Icon(Icons.messenger_outline_sharp),
+                              Text(" Messages"),
+                            ],
+                          ),
+                          onTap: () {
+                            debugPrint("Messages clicked");
+                             Navigator.pop(context);
+                          },
+                        ),
+                        Container(
+                          color: Colors.white,
+                          child: const SizedBox(
+                            height: 5,
+                          ),
+                        ),
+                        ListTile(
+                          title: const Row(
+                            children: [
+                              Icon(Icons.settings),
+                              Text(" Settings"),
+                            ],
+                          ),
+                          onTap: () {
+                            debugPrint("Settings clicked");
+                             Navigator.pop(context);
+                          },
+                        ),
+                        ListTile(
+                          title: const Row(
+                            children: [
+                              Icon(Icons.info),
+                              Text(" Smaple.io"),
+                            ],
+                          ),
+                          onTap: () {
+                            debugPrint("Sample.io clicked");
+                             Navigator.pop(context);
+                          },
+                        ),
+                        ListTile(
+                          title: const Row(
+                            children: [
+                              // TODO: evitar que quebre aundo o nome for muito grande
+                              Text("Marcos Fernando", softWrap: true),
+                              Icon(Icons.exit_to_app),
+                            ],
+                          ),
+                          onTap: () {
+                            debugPrint("Exit clicked");
+                             Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            body: Card(
+              child: Text("Sample Test Page"),
+              color: Colors.blue,
+
             )
         );
       }
