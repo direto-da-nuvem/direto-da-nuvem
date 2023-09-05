7f346d05e303850ae769cf074dbc50c4d30554c7
MarcosFernando <marcosfernando@id.uff.br>
2023-09-05 15:00:06 -0300

    login com o google funcionando

---
+++ lib/ui/views/login_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sample/controllers/login_controller.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<LoginController>(
+        init: LoginController(),
+        builder: (loginController) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Dashboard'),
+              centerTitle: true,
+            ),
+            body: Center(
+              child: ElevatedButton(
+                onPressed: loginController.signInWithGoogle,
+                child: const Text("Google Login"),
+              ),
+            )
+          );
+        }
+    );
+  }
+}
