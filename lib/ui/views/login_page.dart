c9e091c9769e889819ae4911d3e20c37c7d127a0
MarcosFernando <marcosfernando@id.uff.br>
2023-09-12 00:34:59 -0300

    botao de login atualizado

--- lib/ui/views/login_page.dart
+++ lib/ui/views/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:sample/controllers/login_controller.dart';
 
@@ -8,21 +9,39 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<LoginController>(
-        init: Get.put(LoginController()),
-        builder: (loginController) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Login'),
-              centerTitle: true,
-            ),
-            body: Center(
-              child: ElevatedButton(
-                onPressed: loginController.signInWithGoogle,
-                child: const Text("Google Login"),
+      init: Get.put(LoginController()),
+      builder: (loginController) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Sample for Science'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: ElevatedButton(
+              onPressed: loginController.signInWithGoogle,
+              child: const SizedBox(
+                height: 50,
+                width: 180,
+                child: Center(
+                  child: Row(
+                    children: [
+                      Icon(FontAwesomeIcons.google),
+                      SizedBox(width: 15,),
+                      Text("Sign in with Google",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500
+                        )
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            )
-          );
-        }
+            ),
+          )
+        );
+      }
     );
   }
 }
