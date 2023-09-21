b942bc4510c20aca161ec81f4e32c3c8921244a3
GabrielHCRJ <gabrieldomingues@id.uff.br>
2023-09-21 17:07:10 -0300

    Construindo o login

--- lib/ui/views/login_page.dart
+++ lib/ui/views/login_page.dart
@@ -12,35 +12,61 @@ class LoginPage extends StatelessWidget {
       init: Get.put(LoginController()),
       builder: (loginController) {
         return Scaffold(
-          appBar: AppBar(
-            title: const Text('Sample for Science'),
-            centerTitle: true,
-          ),
+
           body: Center(
-            child: ElevatedButton(
-              onPressed: loginController.signInWithGoogle,
-              child: const SizedBox(
-                height: 50,
-                width: 200,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Icon(FontAwesomeIcons.google),
-                    SizedBox(width: 15,),
-                    Text("Sign in with Google",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500
-                      )
-                    ),
-                  ],
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromARGB(100, 199, 209, 241), // Cor da borda vermelha
+                  width: 16.0, // Largura da borda
                 ),
+
+                borderRadius: BorderRadius.circular(20.0)
+
               ),
-            ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Container(
+                        width: 500,
+                        color: Colors.white,
+                        child: Image.asset("assets/logo.png")),
+                    SizedBox(
+                      width: double.infinity,
+                    )]
+                ),
+              )
+
+
+            )
           )
         );
       }
     );
   }
 }
+
+//ElevatedButton(
+//                       onPressed: loginController.signInWithGoogle,
+//                       child: const SizedBox(
+//                         height: 50,
+//                         width: 200,
+//                         child: Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                           children: [
+//                             Icon(FontAwesomeIcons.google),
+//                             SizedBox(width: 15,),
+//                             Text("Sign in with Google",
+//                                 style: TextStyle(
+//                                     color: Colors.white,
+//                                     fontSize: 15,
+//                                     fontWeight: FontWeight.w500
+//                                 )
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                     )
