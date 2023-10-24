5297afab518b3685cd39d3a872105788a7c31ad5
Guilherme <gpaes@id.uff.br>
2023-10-23 21:55:39 -0300

    Edit menu

---
+++ lib/ui/views/edit_page.dart
@@ -0,0 +1,32 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sample/routes/app_pages.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:developer' as developer;
+
+
+class EditPage extends StatefulWidget {
+  const EditPage({Key? key}) : super(key: key);
+
+
+  @override
+  State<EditPage> createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.black38,
+        body: Col
+    );
+  }
+}
+
+
+
